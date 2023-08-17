0dffe144f8b27d6c03a458b7c7f4405abab70370
Jana Schmälzle <jana@familie-schmaelzle.de>
2023-08-17 14:06:14 +0200

    fix form, add catfood.js

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -1,20 +1,12 @@
 import styled from "styled-components";
 
-export default function Button() {
-  return <StyledButton></StyledButton>;
+export default function Button({ children }) {
+  return <StyledButton>{children}</StyledButton>;
 }
 
 const StyledButton = styled.button`
-  padding: 16px 42px;
-  margin-left: 0;
-  border-radius: 10px/20px;
-  box-shadow: 0px 0px 12px -2px rgba(0, 0, 0, 0.5);
-  line-height: 1.25;
-  background: #fc6e51;
-  text-decoration: none;
-  color: black;
-  font-size: 16px;
-  letter-spacing: 0.08em;
-  text-transform: uppercase;
+  font-size: 1.5rem;
+  color: white;
+  background-color: #ff6d60;
   cursor: pointer;
 `;
--- components/NewCatForm/NewCatForm.js
+++ components/NewCatForm/NewCatForm.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import Select from "react-select";
 import { cats } from "@/data/catdata";
+import styled from "styled-components";
+import Button from "../Button/Button";
 
 export default function NewCatForm({ onAddCat }) {
   const [catList, setCatList] = useState(cats);
@@ -38,52 +40,79 @@ export default function NewCatForm({ onAddCat }) {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="inputName">Name: </label>
-      <input
-        type="text"
-        name="inputName"
-        value={cat.name}
-        maxLength={50}
-        onChange={handleChange}
-      />
+    <StyledWrapper>
+      <StyledForm onSubmit={handleSubmit}>
+        <label htmlFor="inputName">Name: </label>
+        <input
+          type="text"
+          name="inputName"
+          value={cat.name}
+          maxLength={50}
+          onChange={handleChange}
+        />
 
-      <label htmlFor="inputAge">Age: </label>
-      <input
-        type="number"
-        name="inputAge"
-        value={cat.age}
-        maxLength={30}
-        onChange={handleChange}
-      />
+        <label htmlFor="inputAge">Age: </label>
+        <input
+          type="number"
+          name="inputAge"
+          value={cat.age}
+          maxLength={30}
+          onChange={handleChange}
+        />
 
-      <label htmlFor="inputAllergies">Allergies: </label>
-      <input
-        type="text"
-        name="inputAllergies"
-        value={cat.allergies}
-        maxLength={50}
-        onChange={handleChange}
-      />
+        <label htmlFor="inputAllergies">Allergies: </label>
+        <input
+          type="text"
+          name="inputAllergies"
+          value={cat.allergies}
+          maxLength={50}
+          onChange={handleChange}
+        />
 
-      <label htmlFor="inputDiseases">Diseases: </label>
-      <input
-        type="text"
-        name="inputDiseases"
-        value={cat.diseases}
-        maxLength={50}
-        onChange={handleChange}
-      />
+        <label htmlFor="inputDiseases">Diseases: </label>
+        <input
+          type="text"
+          name="inputDiseases"
+          value={cat.diseases}
+          maxLength={50}
+          onChange={handleChange}
+        />
 
-      <label htmlFor="inputIntolerances">Intolerances: </label>
-      <input
-        type="text"
-        name="inputIntolerancesw"
-        value={cat.intolerances}
-        maxLength={50}
-        onChange={handleChange}
-      />
-      <button type="submit">Save</button>
-    </form>
+        <label htmlFor="inputIntolerances">Intolerances: </label>
+        <input
+          type="text"
+          name="inputIntolerancesw"
+          value={cat.intolerances}
+          maxLength={50}
+          onChange={handleChange}
+        />
+        <Button type="submit">Save</Button>
+      </StyledForm>
+    </StyledWrapper>
   );
 }
+
+const StyledWrapper = styled.div``;
+
+const StyledForm = styled.form`
+  max-width: 65%;
+  display: flex;
+  flex-direction: column;
+  padding: 0.5rem 1rem;
+  box-shadow: 0px 1px 5px -2px #ff6d60;
+  border-radius: 10px/20px;
+  font-size: 1.3em;
+  background-color: white;
+  gap: 1rem;
+
+  Button {
+    max-width: 30%;
+    text-align: center;
+    background-color: #1d5d9b;
+  }
+
+  label,
+  input {
+    background-color: white;
+  }
+`;
