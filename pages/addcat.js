ee18810495d9aa772bcde72a6e5d0164f01cece7
Jana Schmälzle <jana@familie-schmaelzle.de>
2023-09-02 21:00:22 +0200

    style heading/ back-link addcat

--- pages/addcat.js
+++ pages/addcat.js
@@ -7,7 +7,7 @@ import { styled } from "styled-components";
 export default function NewCatPage({ handleAddCat, forbiddenFoodForCat, cat }) {
   return (
     <>
-      <h1>Add a new cat</h1>
+      <StyledHeading>Add a new cat</StyledHeading>
       <NewCatForm onAddCat={handleAddCat} />
       <StyledLink href={"/"}>Back</StyledLink>
     </>
@@ -16,11 +16,22 @@ export default function NewCatPage({ handleAddCat, forbiddenFoodForCat, cat }) {
 
 const StyledLink = styled(Link)`
   max-width: 20%;
-  background: white;
-  color: red;
+  background: transparent;
+  backdrop-filter: blur(2px);
+  color: #f0caa3;
   font-size: 1em;
   margin: 1em;
   padding: 0.25em 1em;
-  border: 2px solid red;
+  border: 2px solid #f0caa3;
   border-radius: 3px;
+  text-decoration: none;
+  text-align: center;
+`;
+
+const StyledHeading = styled.h1`
+  margin-left: 5%;
+  text-align: center;
+  color: white;
+  letter-spacing: 2.5px;
+  text-shadow: 2px 2px 10px #f0caa3;
 `;
